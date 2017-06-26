ebe9b3e9ea437c960cfa77706a4df2858a0967fc
Duncan Temple Lang <duncan@r-project.org>
2017-06-26 07:21:56 -0700

    access the types from a SolutionSet

--- R/newTypes.R
+++ R/newTypes.R
@@ -4,7 +4,7 @@ function(types, varNames)
 {
   if(length(varNames))
      # already converted the types.         
-     types[paste0(varNames, "_1")]
+     types[paste0(varNames, "")] # "_1"
   else
       list()
 }
