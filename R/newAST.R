190e6df502b0072edcfb3364841228bff3fe15e1
Duncan Temple Lang <duncan@r-project.org>
2017-06-26 17:02:52 -0700

    additional methods for converting AST nodes to R language objects

--- R/newAST.R
+++ R/newAST.R
@@ -25,3 +25,30 @@ setMethod("asRCall", "Call",
         e
       })
 
+
+
+setMethod("asRCall", "Assign",
+          function(x) {
+             e = quote(x <- v)
+             e[[2]] = asRCall(x$write)
+             e[[3]] = asRCall(x$read)             
+             e
+          })
+
+
+setMethod("asRCall", "Replacement",
+         function(x) {
+             e = quote(.tmp[] <- 1)
+             e[[3]] = asRCall(x$args[[length(x$args)]])
+             e[[1]] = as.name("=")
+             
+             a= call("[", as.name(".tmp"))
+             a[[2]] = asRCall(x$args[[1]])
+             idx = x$args[ - unique(c(1, length(x$args) )) ]
+             ridx = lapply(idx, asRCall)
+             a[seq(along = ridx) + 2L] = ridx
+             e[[2]] = a
+             e
+         }
+
+          )
