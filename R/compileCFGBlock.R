7b7ef7bd5d8b801c8ce190c491ac587469a6d65a
Duncan Temple Lang <duncan@r-project.org>
2017-06-28 08:45:25 -0700

    compile any Phi nodes in a block before we compile the body

--- R/compileCFGBlock.R
+++ R/compileCFGBlock.R
@@ -3,6 +3,10 @@ compileCFGBlock =
 function(block, types, ecompiler, builder, llvmFun, llvmBlocks = NULL)
 {
   setInsertBlock(builder, llvmBlocks[[ block$name ]])
+  
+  if(length(block$phi)) 
+     lapply(block$phi, construct_ir, ecompiler, builder, types)
+  
   lapply(block$body, construct_ir, ecompiler, builder, types)
   if(length(block$terminator)) 
      constructTerminator(block$terminator, ecompiler, builder, llvmBlocks)
