528a71fce603670e28e6052b7f952eb980418407
Duncan Temple Lang <duncan@r-project.org>
2017-06-29 18:57:17 -0700

    additional translate_type method and added ... to the methods

--- R/translate_type.R
+++ R/translate_type.R
@@ -6,30 +6,30 @@
 #' @param type (typesys::Type) A type to convert.
 #'
 #' @export
-translate_type = function(type) {
+translate_type = function(type, ...) {
   UseMethod("translate_type")
 }
 
 #' @export
-`translate_type.typesys::RealType` = function(type) {
+`translate_type.typesys::RealType` = function(type, ...) {
   return (Rllvm::DoubleType)
 }
 
-`translate_type.typesys::BooleanType` = function(type) {
-  return (Rllvm::Int8Type) 
+`translate_type.typesys::BooleanType` = function(type, ...) {
+  return (Rllvm::Int32Type) 
 }
 
 #' @export
-`translate_type.typesys::IntegerType` = function(type) {
+`translate_type.typesys::IntegerType` = function(type, ...) {
   return (Rllvm::Int32Type)
 }
 
 
-`translate_type.typesys::ListType` = function(type) {
+`translate_type.typesys::ListType` = function(type, ...) {
   return (Rllvm::VECSXPType)
 }
 
-`translate_type.typesys::ArrayType` = function(type)
+`translate_type.typesys::ArrayType` = function(type, ...)
 {
   elt = type@types
   if(length(elt) == 1) {
@@ -47,6 +47,11 @@ translate_type = function(type) {
 
 
 #' @export
-translate_type.default = function(type) {
+translate_type.default = function(type, ...) {
   browser()
+}
+
+
+`translate_type.typesys::StringType` = function(type, ...) {
+  return (Rllvm::STRSXPType)
 }
