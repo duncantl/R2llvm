5550c5a010e605a249ebc59285a35af05710d0a3
Duncan Temple Lang <duncan@r-project.org>
2017-06-30 08:31:10 -0700

    handle phi nodes and specifically forward refs to instructions that we haven't processed yet.  See the lengthy comment in the code in this commit for an explanation

--- R/construct_ir.R
+++ R/construct_ir.R
@@ -72,43 +72,69 @@ construct_ir.Phi =
 function(node, cmp, helper,  types, .targetType = NULL)
 {
     
-  # Don't insert phis for shadowed globals.
 #XXXX
+# Don't insert phis for shadowed globals.
 #  if ( any(is_global(node$read)) )
 #    return (NULL)
 
+
+  phiName = node$write$name
+  
   type = types[[node$write$name]]
   numIncoming = length(node$blocks)
   phi = Rllvm::createPhi(helper, type, numIncoming, id = node$write$name)
 # Add the incoming. We have them in the node$blocks and in node$read
 
+# We may not have created all of the incoming values for the phi nodes at this point
+# as the order of the CFG may put put creation of the incoming after the Phi node 
+# into which it comes.
+# So we have to make an incomplete Phi node and add its identity and what it is waiting
+# for (i.e. the variable in the Assignment) and when we process that we check to see if
+# we need to add it to any of the incomplete Phi nodes.
+
+  
   mapply(function(var, block) {
            val = cmp$.phiVarInstructions[[ var$name ]]
-# Are we supposed to createLoad(val). Get a "Instruction does not dominate all uses!" error.
+
+           if(is.null(val)) {
+                # so the intruction has not been processed yet.
+                # We create a dummy Value and then arrange to replace
+                # it with the actual instruction at the end of the module/
+                # routine construction. This is the purpose of
+                # the llvm function replaceAllUsesWith()
+                # This approach is what llvm uses itself when we create
+                # the c++ API code from a .ll file. i.e., we are copying
+                # that directly.
+             val = .Call("R_createFwdRef_for_phi", type)
+             cmp$.phiForwardRefs[[ var$name ]] = val
+           }
+
            addIncoming(phi, val, block)
          }, node$read, cmp$blocks[node$blocks])
 
   if(node$write$name %in% names(cmp$.phiVarInstructions)) 
-     cmp$.phiVarInstructions[[ node$write$name ]] <- phi
+     cmp$.phiVarInstructions[[ phiName ]] <- phi
   else
-     cmp$.allocVars[[ node$write$name ]] <- phi
-
-#  e = substitute(x <- y, list(x = as.name(node$write$name), y = phi))
-#  v = `compile.=`(e, cmp, helper)
+     cmp$.allocVars[[ phiName ]] <- phi
+  
   phi
 }
 
 construct_ir.Symbol =
 function(node, cmp, helper,  types, .targetType = NULL)
 {
-#browser()    
+browser()    
   v = cmp$getAlloc(node$name)
   if(is.null(v)) {
      v = cmp$.params[[ node$name ]]
      return(v)
   }
-#  v  
-  helper$createLoad( v )
+
+ #XXX Do we only load this if it is an AllocaInst?
+ if(!is(v, "PHINode"))
+    helper$createLoad( v )
+ else
+    v
 }
 
 construct_ir.Integer =
@@ -132,7 +158,6 @@ function(node, cmp, helper,  types, .targetType = NULL)
 construct_ir.Numeric =
 function(node, cmp, helper,  types, .targetType = NULL)
 {
-browser()    
    createConstant(helper, node$value)
 }
 
