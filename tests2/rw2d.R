887504e8a6ed6ac4d01bc3328ec3b9da03224ff3
Duncan Temple Lang <duncan@r-project.org>
2017-07-01 18:42:20 -0700

    updates to the rw2d example code to try to simplify what the current compiler code has to deal with

--- tests2/rw2d.R
+++ tests2/rw2d.R
@@ -4,23 +4,33 @@ rw2d = function(n = 100L) {
   ypos = integer(n) # xpos
   for(i in 2L:n) {
     # Decide whether we are moving horizontally or vertically.
-    if (runif(1) > .5)
+    if (Rf_runif(0, 1) > .5)
       delta = 1L
     else
       delta = -1L
 
-    if (runif(1) > .5) {
-      xpos[i] = xpos[i-1] + delta
-      ypos[i] = ypos[i-1]
+    prev = i-1L
+    if (Rf_runif(0, 1) > .5) {
+      xpos[i] = xpos[prev] + delta
+      ypos[i] = ypos[prev]
     }
     else {
-      xpos[i] = xpos[i-1]
-      ypos[i] = ypos[i-1] + delta
+      xpos[i] = xpos[prev]
+      ypos[i] = ypos[prev] + delta
     }
   }
   list(x = xpos, y = ypos)
 }
 
-cfg = rstatic::to_cfg(rw2d) # infer_types() doesn't work if use ssa = FALSE)
-tt = RTypeInference::infer_types(cfg)
+#cfg = rstatic::to_cfg(rw2d) # infer_types() doesn't work if use ssa = FALSE)
+#tt = RTypeInference::infer_types(cfg)
 
+library(R2llvm)
+
+ast = rstatic::to_ast(rw2d)
+R2llvm:::rewriteAST(ast)
+cfg = rstatic::to_cfg(ast) 
+types = RTypeInference::infer_types(cfg, init = list(prev = typesys::IntegerType()))
+
+fc = compileFunction(ast, cfg, types, name = "rw2d", .rewriteAST = FALSE)
+#fc = compileFunction(rw2d)
