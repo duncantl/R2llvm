898de45b349365a6729a038543f9d44421d0d1d4
Duncan Temple Lang <duncan@r-project.org>
2017-07-01 18:40:29 -0700

    check the results and add timings.

--- tests2/nimble_rcjit.R
+++ tests2/nimble_rcjit.R
@@ -12,5 +12,21 @@ logistic_map <- function(start = 1.0, scale = 1.0, steps = 1000L) {
 library(R2llvm)
 fc = compileFunction(logistic_map)
 
-#a1 = logistic_map(0.1, 3.6, 1e6L)    
-.llvm(fc, 0.1, 3.6, 1e6L)
+a = logistic_map(0.1, 3.6, 1e6L)    
+b = .llvm(fc, 0.1, 3.6, 1e6L)
+stopifnot(identical(a,b))
+
+
+if(FALSE) {
+library(microbenchmark)
+N = 1e6
+mb = microbenchmark(.llvm(fc, 0.1, 3.6, N), logistic_map(0.1, 3.6, N), times = 20)
+print(with(mb, do.call(`/`, rev(tapply(time, expr, summary)))))
+
+ee = ExecutionEngine(fc)
+# Warm this up with an initial call.
+tm2 = system.time(a2 <- .llvm(fc, 0.1, 3.6, N, .ee = ee))
+mb.ee = microbenchmark(.llvm(fc, 0.1, 3.6, N, .ee = ee), logistic_map(0.1, 3.6, N), times = 20)
+print(with(mb.ee, do.call(`/`, rev(tapply(time, expr, summary)))))
+}
+
